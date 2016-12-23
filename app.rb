14910a5165f83f4925fb25e837e623f539e9b7bd
Ryne <hansonryne@gmail.com>
2016-12-23 15:35:45 -0600

    Changed variable names to match Ruby Style
    Signed-off-by: Ryne <hansonryne@gmail.com>

--- app.rb
+++ app.rb
@@ -20,11 +20,11 @@ puts 'Welcome to the 7 Wonders score calculator'
 sleep 1
 
 #This section starts the calculator by asking how many players there were in the game
-isRight = false
-while isRight == false
+is_right = false
+until is_right
   puts 'How many players were there in the game?'
-  $numPlayers = gets.chomp.to_i
-  puts "You had #{$numPlayers} players."
+  $num_players = gets.chomp.to_i
+  puts "You had #{$num_players} players."
   sleep 1
   puts 'Is that right?'
   sleep 1
@@ -32,128 +32,128 @@ while isRight == false
   puts '2. No'
   answer = gets.chomp.to_i
   if answer == 1
-    print "Starting calculator with #{$numPlayers} players."
+    print "Starting calculator with #{$num_players} players."
     3.times do
       print '.'
       sleep 1
     end
     print "\n"
-    isRight = true
+    is_right = true
   else
     print 'Starting over'
     3.times do
       print '.'
       sleep 1
     end
-      print "\n"
+    print "\n"
   end
 end
 
-playerArray = []
+player_array = []
 
 #Set player names
-for i in 1..$numPlayers
+for i in 1..$num_players
   puts "What is player #{i}'s name?"
-  playerArray[i-1] = Player.new("#{gets.chomp}")
-  puts "You entered #{playerArray[i-1].name}"
+  player_array[i-1] = Player.new("#{gets.chomp}")
+  puts "You entered #{player_array[i-1].name}"
   print "\n\n"
 end
 
 #Set military scores
-for i in 1..$numPlayers
-  puts "What is #{playerArray[i-1].name}'s military score?"
-  playerArray[i-1].redScore = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].redScore}"
+for i in 1..$num_players
+  puts "What is #{player_array[i-1].name}'s military score?"
+  player_array[i-1].redScore = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].redScore}"
   print "\n\n"
 end
 
 #Set coin scores
-for i in 1..$numPlayers
-  puts "How many points for coins did #{playerArray[i-1].name} get?"
-  playerArray[i-1].coinScore = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].coinScore}"
+for i in 1..$num_players
+  puts "How many points for coins did #{player_array[i-1].name} get?"
+  player_array[i-1].coinScore = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].coinScore}"
   print "\n\n"
 end
 
 #Set wonder scores
-for i in 1..$numPlayers
-  puts "What is #{playerArray[i-1].name}'s wonder score?"
-  playerArray[i-1].wonderScore = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].wonderScore}"
+for i in 1..$num_players
+  puts "What is #{player_array[i-1].name}'s wonder score?"
+  player_array[i-1].wonderScore = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].wonderScore}"
   print "\n\n"
 end
 
 #Set blue card score
-for i in 1..$numPlayers
-  puts "How many blue card points did #{playerArray[i-1].name} get?"
-  playerArray[i-1].blueScore = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].blueScore}"
+for i in 1..$num_players
+  puts "How many blue card points did #{player_array[i-1].name} get?"
+  player_array[i-1].blueScore = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].blueScore}"
   print "\n\n"
 end
 
 #Set yellow card score
-for i in 1..$numPlayers
-  puts "How many yellow card points did #{playerArray[i-1].name} get?"
-  playerArray[i-1].yellowScore = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].yellowScore}"
+for i in 1..$num_players
+  puts "How many yellow card points did #{player_array[i-1].name} get?"
+  player_array[i-1].yellowScore = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].yellowScore}"
   print "\n\n"
 end
 
 #Set purple card score
-for i in 1..$numPlayers
-  puts "How many purple card points did #{playerArray[i-1].name} get?"
-  playerArray[i-1].purpleScore = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].purpleScore}"
+for i in 1..$num_players
+  puts "How many purple card points did #{player_array[i-1].name} get?"
+  player_array[i-1].purpleScore = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].purpleScore}"
   print "\n\n"
 end
 
 #Science is a little more complicated
-for i in 1..$numPlayers
-  puts "How many cogs did #{playerArray[i-1].name} have?"
-  playerArray[i-1].cogs = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].cogs}"
+for i in 1..$num_players
+  puts "How many cogs did #{player_array[i-1].name} have?"
+  player_array[i-1].cogs = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].cogs}"
   print "\n\n"
 
-  puts "How many compasses did #{playerArray[i-1].name} have?"
-  playerArray[i-1].compass = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].compass}"
+  puts "How many compasses did #{player_array[i-1].name} have?"
+  player_array[i-1].compass = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].compass}"
   print "\n\n"
 
-  puts "How many tablets did #{playerArray[i-1].name} have?"
-  playerArray[i-1].tablets = gets.chomp.to_i
-  puts "You entered #{playerArray[i-1].tablets}"
+  puts "How many tablets did #{player_array[i-1].name} have?"
+  player_array[i-1].tablets = gets.chomp.to_i
+  puts "You entered #{player_array[i-1].tablets}"
   print "\n\n"
 
 #Individual score for the gear cards (I don't know why I called them cogs but it was too far gone by the time I noticed)
-  playerArray[i-1].scoreCogs
+  player_array[i-1].scoreCogs
 #Individual score for the compass cards
-  playerArray[i-1].scoreCompass
+  player_array[i-1].scoreCompass
 #Individual score for the tablet cards
-  playerArray[i-1].scoreTablet
+  player_array[i-1].scoreTablet
 #Individual score for sets of science cards
-  playerArray[i-1].scoreSets
+  player_array[i-1].scoreSets
 
 #Totals all the science and prints the result
-  puts "#{playerArray[i-1].name}'s science score is #{playerArray[i-1].scoreScience}"
+  puts "#{player_array[i-1].name}'s science score is #{player_array[i-1].scoreScience}"
   print "\n\n"
 end
 
 #Calculate total score
-playerArray.each do |x|
+player_array.each do |x|
   puts "#{x.name} scored #{x.sayTotalScore} points."
   print "\n\n"
 end
 
 #Tabulate results
-tableRows = []
-playerArray.each do |x|
-  tableRows << [x.name, x.redScore, x.coinScore, x.wonderScore, x.blueScore, x.yellowScore, x.purpleScore, x.scienceScore, x.totalScore]
+table_rows = []
+player_array.each do |x|
+  table_rows << [x.name, x.redScore, x.coinScore, x.wonderScore, x.blueScore, x.yellowScore, x.purpleScore, x.scienceScore, x.totalScore]
 end
 
 table = Terminal::Table.new
 table.title = 'Final Scores'
-table.headings = ['Player', 'Military', 'Coin', 'Wonder', 'Blue', 'Yellow', 'Purple', 'Science', 'Total']
-table.rows = tableRows
+table.headings = %w(Player Military Coin Wonder Blue Yellow Purple Science Total)
+table.rows = table_rows
 table.style = {:all_separators => true}
 
 
